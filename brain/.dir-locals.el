7af6a3e3191401d1bd56c1930c232b1cef644df7
Accacio <raccacio@poli.ufrj.br>
2021-07-21 16:38:41 +0200

    add org hugo base dir local variable

---
+++ brain/.dir-locals.el
@@ -0,0 +1,8 @@
+(
+ (org-mode . (
+              (org-hugo-base-dir . "~/git/brain/")
+              (org-hugo-section . "notes")
+              ))
+ ("daily" . ((org-hugo-base-dir . nil)))
+ ("private" . ((org-hugo-base-dir . nil)))
+ )
