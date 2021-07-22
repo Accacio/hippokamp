d3c3ced70cc1cb1689031b6c1265340bdfb306bc
Accacio <raccacio@poli.ufrj.br>
2021-07-22 15:31:14 +0200

    modify order and other

--- brain/.dir-locals.el
+++ brain/.dir-locals.el
@@ -1,6 +1,7 @@
 (
  (org-mode . (
               (org-hugo-base-dir . "~/git/brain/")
+              (org-hugo-baseURL . "/brain")
               (org-hugo-section . "notes")
               ))
  ("daily" . ((org-hugo-base-dir . nil)))
